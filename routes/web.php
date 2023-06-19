44a8893caebc2d6657a4463f495ce174265e8419
GerrysatriaH <gerrysatria10@gmail.com>
2023-06-19 09:25:22 +0700

    routes & view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Gerry Satria Halim",
+        "email" => "gerrysatria10@gmail.com",
+        "image" => "gerry.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
