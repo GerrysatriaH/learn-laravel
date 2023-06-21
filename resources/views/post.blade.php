c3f35e2aa6ca581ed2b97515959ba6b176a3e5cb
GerrysatriaH <gerrysatria10@gmail.com>
2023-06-21 14:16:57 +0700

    Blade Escape Character

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,14 @@
 
 @section('container')
     <article>
-        <h2>{{ $posts['title'] }}</h2>
-        <h5>By : {{ $posts['author'] }}</h5>
-        <p>{{ $posts['body'] }}</p>
+        <h2>{{ $posts['name'] }}</h2>
+        <h5>{{ $posts['price'] }}</h5>
+
+        {{-- With Blade Escape Character--}}
+        <p>{{ $posts['desc'] }}</p>
+
+        {{-- Without Blade Escape Character, So if there is tag html or SQL query in value, then tag or SQL query will be execute by program--}}
+        {{-- {!! $posts['desc'] !!} --}}
     </article>
+    <a href="/product">Back</a>
 @endsection
