99068f524ddbff5da63b6e3ef5f88df4bd43c97e
GerrysatriaH <gerrysatria10@gmail.com>
2023-06-26 23:12:20 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Post::create([
+            'category_id' => 1,
+            'name' => 'Sari Roti',
+            'slug' => 'sari-roti',
+            'price'=> 10000,
+            'qty'  => 30
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Post::create([
+            'category_id' => 2,
+            'name' => 'Fruity',
+            'slug' => 'fruity',
+            'price'=> 4000,
+            'qty'  => 10
+        ]);
+
+        Post::create([
+            'category_id' => 1,
+            'name' => 'Indomie',
+            'slug' => 'indomie',
+            'price'=> 3500,
+            'qty'  => 20
+        ]);
+
+        Category::create([
+            'name' => 'Makanan',
+            'slug' => 'makanan'
+        ]);
+        
+        Category::create([
+            'name' => 'Minuman',
+            'slug' => 'minuman'
+        ]);
+
+        User::create([
+            'name' => 'Gerry Satria Halim',
+            'email' => 'gerrysatria@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -11,7 +11,8 @@
                     {{-- With Blade Escape Character--}}
                     <h5 class="card-title">{{ $post['name'] }}</h5>
                     <p class="card-text">Rp. {{ $post['price'] }}</p>
-                    <p class="card-text">Kategori : <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }} </a></p>
+                    <p class="card-text">Kategori : 
+                        <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }} </a></p>
                     <p class="card-text"><small class="text-body-secondary">Stok : {{ $post['qty'] }}</small></p>
                     {{-- Without Blade Escape Character, So if there is tag html or SQL query in value, then tag or SQL query will be execute by program--}}
                     {{-- Example : {!! $post['desc'] !!} --}}
